e0925acb32a9412a0732c3f7162e60acf5be8693
Majid Jaidi <majid.jaidi@gmail.com>
2020-02-13 11:07:21 +0100

    added authorized api origin requests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -19,7 +19,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins %r{\Ahttp://localhost:\d+\z}
+    origins 'http://localhost:3001', 'https://modest-lamarr-603e15.netlify.com'
     resource '/*',
              headers: :any,
              methods: :any,
